d8431e7b98443ca8e90845c4b57287da4bb22d23
Illya Nesterenko <illya_nesterenko@ukr.net>
2020-01-28 14:21:52 +0200

    first commit

---
+++ dz2/1.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+c = 25
+d = 50
+
+print(a + b)
+print(c - a)
+print(b * c)
+print(d / b)
+
---
+++ dz2/3.py
@@ -0,0 +1,8 @@
+import random
+
+list = [random.randint(0, 100) for i in range(10)]
+
+list.pop()
+
+print(list)
+
